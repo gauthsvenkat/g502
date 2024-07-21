8a824032c5bc058b43c2d16d189c0e9c295d97ff
Gautham <gauthsvenkat@gmail.com>
2024-07-21 22:22:27 +0200

    One gaming profile to rule them all

--- default.sh
+++ default.sh
@@ -2,9 +2,10 @@
 
 # Get the device name
 device=$(ratbagctl list | grep -E 'Logitech G502|Logitech G502 LIGHTSPEED Wireless Gaming Mouse' | awk -F: '{print $2}' | awk '{$1=$1};1')
+profile=0
 
-# Set the profile (0 is default)
-ratbagctl "$device" profile active set 0 --nocommit
+# Set the profile
+ratbagctl "$device" profile active set $profile --nocommit
 
 # Set the DPI
 ratbagctl "$device" dpi set 1200 --nocommit
@@ -16,14 +17,16 @@ ratbagctl "$device" rate set 125 --nocommit
 ratbagctl "$device" button 3 action set macro +KEY_LEFTCTRL +KEY_LEFTSHIFT KEY_TAB -KEY_LEFTSHIFT -KEY_LEFTCTRL --nocommit
 ratbagctl "$device" button 4 action set macro +KEY_LEFTCTRL KEY_TAB -KEY_LEFTCTRL --nocommit
 ratbagctl "$device" button 5 action set macro +KEY_LEFTCTRL KEY_W -KEY_LEFTCTRL --nocommit
-
 # Index back, front and mid
 ratbagctl "$device" button 6 action set macro KEY_SYSRQ --nocommit
 ratbagctl "$device" button 7 action set button 3 --nocommit
 ratbagctl "$device" button 8 action set special "profile-cycle-up" --nocommit
-
 # Scroll wheel left and right
 ratbagctl "$device" button 9 action set special "wheel-right" --nocommit
 ratbagctl "$device" button 10 action set special "wheel-left"
+
+# Set LED behavior. led 0 is the logo, 1 is the DPI lights
+ratbagctl "$device" led 0 set mode off --nocommit
+ratbagctl "$device" led 1 set mode on color 000000
 
 echo "Default profile set for $device"
--- gaming.sh
+++ gaming.sh
@@ -2,9 +2,10 @@
 
 # Get the device name
 device=$(ratbagctl list | grep -E 'Logitech G502|Logitech G502 LIGHTSPEED Wireless Gaming Mouse' | awk -F: '{print $2}' | awk '{$1=$1};1')
+profile=1
 
 # Set the profile
-ratbagctl "$device" profile active set 1 --nocommit
+ratbagctl "$device" profile active set $profile --nocommit
 
 # Set the DPI
 ratbagctl "$device" dpi set 1200 --nocommit
@@ -13,17 +14,19 @@ ratbagctl "$device" dpi set 1200 --nocommit
 ratbagctl "$device" rate set 125 --nocommit
 
 # Thumb back, front and mid
-ratbagctl "$device" button 3 action set macro KEY_2 --nocommit
-ratbagctl "$device" button 4 action set macro KEY_1 --nocommit
-ratbagctl "$device" button 5 action set macro KEY_M --nocommit
-
+ratbagctl "$device" button 3 action set macro KEY_0 --nocommit
+ratbagctl "$device" button 4 action set macro KEY_9 --nocommit
+ratbagctl "$device" button 5 action set macro KEY_8 --nocommit
 # Index back, front and mid
-ratbagctl "$device" button 6 action set macro KEY_X --nocommit
-ratbagctl "$device" button 7 action set button 3 --nocommit
+ratbagctl "$device" button 6 action set macro KEY_7 --nocommit
+ratbagctl "$device" button 7 action set macro KEY_6 --nocommit
 ratbagctl "$device" button 8 action set special "profile-cycle-up" --nocommit
-
 # Scroll wheel left and right
 ratbagctl "$device" button 9 action set special "wheel-right" --nocommit
 ratbagctl "$device" button 10 action set special "wheel-left"
 
-echo "DRG profile set for $device"
+# Set LED behavior. led 0 is the logo, 1 is the DPI lights
+ratbagctl "$device" led 0 set mode cycle duration 10000 brightness 255 --nocommit
+ratbagctl "$device" led 1 set mode on color 000000
+
+echo "Gaming profile set for $device"
